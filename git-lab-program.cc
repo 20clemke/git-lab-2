3f95aa860c73ea7a80f2b06745d884f9bb262667
20clemke <cl592619@ohio.edu>
2022-01-21 14:35:06 -0500

    Lab program file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Collin Lemke
+ *        Date: 1/21/22
+ * Description: Says a simple statement about Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
